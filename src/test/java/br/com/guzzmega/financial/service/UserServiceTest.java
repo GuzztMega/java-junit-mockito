220d5be2511ec814692d4655a62e59d802484ab1
guzzmega <guzzt.mega@gmail.com>
2023-09-17 17:06:23 -0300

    Added ExtendWith annotation and mustRejectExistentUserOnSave teste

--- src/test/java/br/com/guzzmega/financial/service/UserServiceTest.java
+++ src/test/java/br/com/guzzmega/financial/service/UserServiceTest.java
@@ -2,26 +2,35 @@ package br.com.guzzmega.financial.service;
 
 import br.com.guzzmega.financial.domain.User;
 import br.com.guzzmega.financial.domain.builders.UserBuilder;
+import br.com.guzzmega.financial.exception.ValidationException;
 import br.com.guzzmega.financial.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
+    @InjectMocks
     private UserService userService;
+
+    @Mock
     private UserRepository userRepository;
 
-    @BeforeEach
-    public void setup(){
-        userRepository = Mockito.mock(UserRepository.class);
-        userService = new UserService(userRepository);
-    }
-
     /**
+     // Redundant because @ExtendWith(MockitoExtension.class)
+     @BeforeEach
+     public void setup(){
+     userRepository = Mockito.mock(UserRepository.class);
+     userService = new UserService(userRepository);
+     }
+
      // Extra validations [high coupling!]
      @AfterEach
      public void tearDown(){
@@ -33,8 +42,7 @@ public class UserServiceTest {
     public void musReturnEmptyWhenInexistentUser(){
         /**
          Mockito default behavior [no need for implementation]
-         Mockito.when(userRepository.findUserByEmail("joao@gmail.com"))
-                .thenReturn(Optional.empty());
+         Mockito.when(userRepository.findUserByEmail("joao@gmail.com")).thenReturn(Optional.empty());
         **/
 
         Optional<User> user = userService.findUserByEmail("joao@gmail.com");
@@ -81,5 +89,19 @@ public class UserServiceTest {
          // Extra validation [high coupling!]
          Mockito.verify(userRepository).save(savingUser);
         **/
+    }
+
+    @Test
+    public void mustRejectExistentUserOnSave(){
+        User savingUser = UserBuilder.getOneUser().withId(null).build();
+
+        Mockito.when(userRepository.findUserByEmail(savingUser.getEmail()))
+                .thenReturn(Optional.of(UserBuilder.getOneUser().build()));
+
+        ValidationException exception = Assertions.assertThrows(ValidationException.class, () ->
+                userService.save(savingUser));
+        Assertions.assertTrue(exception.getMessage().endsWith("is already in use"));
+
+        Mockito.verify(userRepository, Mockito.never()).save(savingUser);
     }
 }
